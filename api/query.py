687ada83aa8193a155d5d00e1e8e9f64a9814523
ChrisMJones1 <chrisjns679@gmail.com>
2020-03-02 02:02:33 -0500

    Added flask API hotmapping, hotmap caching, time of day tracking
    Added flask API to display map and respond to movements of the map, and display the proper data to heatmap, while adjusting for the time zone in LA and the currently cached points on the client's browser.

---
+++ api/query.py
@@ -0,0 +1,121 @@
+from flask import Flask, request, render_template, jsonify, send_from_directory
+import requests
+import numpy as np
+import pandas as pd
+
+# from flask_cors import CORS
+
+
+import pyarrow
+from datetime import datetime
+
+# import os
+# os.environ['PATH']
+
+
+app = Flask(__name__, static_folder='../')
+
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'location_test_map_1200_to_1259.html')
+# CORS(app)
+
+# @app.route('/')
+# def index():
+#     return "Hello, World!"
+
+#
+# @app.route('/')
+# def index():
+#     return print('connected')
+
+
+@app.route('/map', methods=['POST'])
+def process():
+    bounds = request.get_json(force=True)
+
+    starttime = (str(bounds['time']) + "00").zfill(4)
+    endtime = starttime[:2] + "59"
+    parquetname = "C:/ticketdodger/python/data/mini/mintickets_" + starttime + "_to_" + endtime + ".parquet"
+    df = pd.read_parquet(parquetname)
+    cache_df = df;
+    # new_df = df;
+    # cache_df = df;
+    # print(df)
+
+    # print(bounds)
+    try:
+        minlat = bounds['latlng']['_southWest']['lat']
+    except TypeError:
+        print(bounds)
+        print(bounds['_southWest'])
+    maxlat = bounds['latlng']['_northEast']['lat']
+    minlong = bounds['latlng']['_southWest']['lng']
+    maxlong = bounds['latlng']['_northEast']['lng']
+    # print("min long = " + str(minlong))
+    # print("max long = " + str(maxlong))
+    #
+    # print("min lat = " + str(minlat))
+    # print("max lat = " + str(maxlat))
+
+    df_longfiltermin = (df['new_long'] >= minlong)
+    df = df[df_longfiltermin]
+    df_longfiltermax = (df['new_long'] <= maxlong)
+    df = df[df_longfiltermax]
+
+    # print(df)
+
+    df_latfiltermin = (df['new_lat'] >= minlat)
+    df = df[df_latfiltermin]
+    df_latfiltermax = (df['new_lat'] <= maxlat)
+    df = df[df_latfiltermax]
+
+    #do the same, but grab the cached entries
+
+    try:
+        minlat = bounds['cache']['_southWest']['lat']
+    except TypeError:
+        print(bounds)
+        print(bounds['_southWest'])
+    maxlat = bounds['cache']['_northEast']['lat']
+    minlong = bounds['cache']['_southWest']['lng']
+    maxlong = bounds['cache']['_northEast']['lng']
+    # print("min long = " + str(minlong))
+    # print("max long = " + str(maxlong))
+    #
+    # print("min lat = " + str(minlat))
+    # print("max lat = " + str(maxlat))
+
+    df_longfiltermin = (cache_df['new_long'] >= minlong)
+    cache_df = cache_df[df_longfiltermin]
+    df_longfiltermax = (cache_df['new_long'] <= maxlong)
+    cache_df = cache_df[df_longfiltermax]
+
+    # print(df)
+
+    df_latfiltermin = (cache_df['new_lat'] >= minlat)
+    cache_df = cache_df[df_latfiltermin]
+    df_latfiltermax = (cache_df['new_lat'] <= maxlat)
+    cache_df = cache_df[df_latfiltermax]
+
+    df = (pd.merge(df, cache_df, indicator=True, how='outer')
+         .query('_merge=="left_only"')
+         .drop('_merge', axis=1))
+    # print(df)
+
+    x = df['new_lat'].values
+    y = df['new_long'].values
+
+    mapdata = np.transpose([x, y]).tolist()
+    # print(mapdata)
+
+    return jsonify({'latlongs': mapdata})
+    # output = firstName + lastName
+    # if firstName and lastName:
+    #     return jsonify({'output':'Full Name: ' + output})
+
+    return jsonify({'error' : 'Missing data!'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ python/geoconvert.py
@@ -0,0 +1,40 @@
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from scipy.stats import gaussian_kde
+# import seaborn as sns
+from pyproj import Proj, transform
+# import folium
+# from folium.plugins import HeatMap
+# import geopandas as gpd
+# import pyarrow
+
+from datetime import datetime
+
+# hmap = folium.Map(location=[34, -118], zoom_start=9)
+
+
+from itertools import product
+from string import ascii_uppercase
+from matplotlib import patheffects
+
+# plt.style.use('seaborn')
+
+# plt.close('all')
+
+inProj = Proj("esri:102645", preserve_units=False)
+outProj = Proj("epsg:4326") # WGS84 in lat long
+y1, x1 =  (560301.731, 1976879.710)
+print(transform(inProj,outProj,x1,y1))
+
+# Center LA 34°02'36.1"N 118°15'01.4"W 34.04336111111111 -118.25038888888889
+minlat = (1976879.710  - 1250)
+maxlat = (1976879.710 + 1250)
+minlong = (560301.731  - 1250)
+maxlong = (560301.731 + 1250)
+
+y1, x1 =  (minlong, maxlat)
+print(transform(inProj,outProj,x1,y1))
+
+y1, x1 =  (maxlong, maxlat)
+print(transform(inProj,outProj,x1,y1))
