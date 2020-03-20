be4953d8753248ca6cf565f3180a04a9c99cc151
chris_jones <chrisjns679@gmail.com>
2020-03-20 16:51:59 -0400

    added live production version
    Added live site production code files to repositiory

---
+++ live_production/ticketdodger/splash.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    var OpenStreetMapProvider = window.GeoSearch.OpenStreetMapProvider;
+    var DateTime = luxon.DateTime;
+    var user_time = DateTime.local().setZone('America/Los_Angeles');
+    var user_hour = user_time.hour;
+
+    var address_input;
+
+
+
+    $("#search_form").submit(search);
+
+    function search(event) {
+        address_input = $("#search_input").val();
+
+        // import
+        // import { OpenStreetMapProvider } from 'leaflet-geosearch';
+
+        // setup
+        const provider = new OpenStreetMapProvider();
+
+        // search
+        provider.search({query: address_input}).then(function (result) {
+            // address_input = result;
+            // result = JSON.parse(result);
+            result = result[0];
+            result.x = parseFloat(result.x);
+            result.y = parseFloat(result.y);
+             return render_search(result);
+        });
+        event.preventDefault();
+    }
+    function render_search(result) {
+        if (result.y >= 33.7030139 && result.y <= 34.7478410 && result.x <= -117.6972698 && result.x >= -118.9409686) {
+            $("#placename").val(address_input);
+            $("#lat").val(result.y);
+            $("#long").val(result.x);
+            $("#hidden_form").submit();
+        } else {
+
+            $("#error_message").removeClass("d-none");
+            $("#form_glow").removeClass("glow");
+            $("#form_glow").addClass("glow-red");
+            return false;
+        }
+    }
+});
